bf51193982631c2194fc6ef930571a355912eb13
TaylorAnderson33 <114946786+TaylorAnderson33@users.noreply.github.com>
2023-02-08 12:50:08 -0700

    Update section 3.R

--- 6 visualization day 1/section 3.R
+++ 6 visualization day 1/section 3.R
@@ -124,13 +124,51 @@ tips <- read_csv('https://www.dropbox.com/s/rydxlxdarjdoj7a/tips.csv?dl=1')
 
 # Let's plot tip_percentage vs. total_bill,
 # then split that across lots of categories
+tips %>% count(size)
 tips %>% 
   mutate(bad_smoker_tip = ifelse(smoker == 'Yes', tip*100, tip)) %>% 
-  mutate(bad_day_total = ifelse(day %in% c('Sun', 'Sat'), total_bill*1000, total_bill))
+  #mutate(bad_day_total = ifelse(day %in% c('Sun', 'Sat'), total_bill*1000, total_bill))
+  ggplot(aes(x = total_bill, y = bad_smoker_tip, color = sex)) +
+  geom_point(size =3) +
+  facet_wrap(~smoker, scales = 'free_y')
 
+
+
+
+tips %>%
+  mutate(tip_perc = tip/total_bill) %>% 
+  mutate(bad_smoker_tip = ifelse(smoker == 'Yes', tip*100, tip)) %>% 
+  mutate(bad_day_total = ifelse(day %in% c('Sun', 'Sat'), total_bill*1000, total_bill)) %>% 
+  ggplot(aes(x = bad_day_total, y = bad_smoker_tip, color = sex)) +
+  geom_point(size =3) +
+  facet_grid(day~sex, scales = 'free')
+  
+  
 
 econ <- read_csv('https://www.dropbox.com/s/8bq9rw0rk46hru2/econ.csv?dl=1')
 
 # Let's plot two measures over time: savings rate & unemployment weeks
 # It's easiest if we pivot to make this work
+econ %>% 
+  select(date, savings_rate, unempl_weeks) %>% 
+  #filter(date < lubridate::ymd('1975-01-01')) %>% 
+  pivot_longer(!date, 
+               names_to = 'Measure',
+               values_to = 'Rate') %>% 
+ggplot(aes(x = date, y = Rate, color = Measure)) +
+geom_line() +
+facet_wrap(~Measure, ncol=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
